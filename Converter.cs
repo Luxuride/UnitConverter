82410060ef52d3a6bb81f7890a4792d7218742d4
Lukáš <luxuridegaming@gmail.com>
2019-12-19 15:52:21 +0100

    Refactor

--- Converter.cs
+++ Converter.cs
@@ -11,23 +11,21 @@ namespace UnitConverter
         public Converter(string config) {
             _config = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, double>>>(config);
         }
-        public double ConvertSimple(string from, string to, double number){
+        public double Convert(string from, string to, double number){
             string advancedType;
-            
-            
             string fromType = _config.Where(x => x.Value.ContainsKey(from)).First().Key;
             string toType = _config.Where(x => x.Value.ContainsKey(to)).First().Key;
             if (_config.ContainsKey($"{fromType}to{toType}") || _config.ContainsKey($"{toType}to{fromType}")){
                 advancedType = _config.Where(x => x.Key == $"{fromType}to{toType}" || x.Key == $"{toType}to{fromType}").First().Key;
                 string fromBasic = _config[advancedType].Where(x => _config[fromType].ContainsKey(x.Key)).First().Key;
                 string toBasic = _config[advancedType].Where(x => _config[toType].ContainsKey(x.Key)).First().Key;
-                return ConvertSimple(toBasic, to, ConvertSimple(from, fromBasic, number)) * _config[advancedType][fromBasic] / _config[advancedType][toBasic];
+                return Convert(toBasic, to, Convert(from, fromBasic, number)) * _config[advancedType][fromBasic] / _config[advancedType][toBasic];
             }
             else if(fromType == toType) {
                 return number / _config[toType][to] * _config[fromType][from];
             }
             throw new ArgumentException($"Can't convert {from} to {to}");
         }
-        public double ConvertAdvanced(string fromDistance, string toDistance, string fromTime, string toTime, double number) => number * ConvertSimple(fromDistance, toDistance, 1) / ConvertSimple(fromTime, toTime, 1);
+        public double Convert(string fromDistance, string toDistance, string fromTime, string toTime, double number) => number * Convert(fromDistance, toDistance, 1) / Convert(fromTime, toTime, 1);
     }
 }
--- Program.cs
+++ Program.cs
@@ -21,11 +21,11 @@ namespace UnitConverter
                         if(values[1].Split('/').Length == 2 && values[2].Split('/').Length == 2) {
                             var values1 = values[1].Split('/');
                             var values2 = values[2].Split('/');
-                            Console.WriteLine($"{values[0]} {values[1]} -> {converter.ConvertAdvanced(values1[0], values2[0], values1[1], values2[1], number)} {values[2]}");
+                            Console.WriteLine($"{values[0]} {values[1]} -> {converter.Convert(values1[0], values2[0], values1[1], values2[1], number)} {values[2]}");
                         } 
                         // Convert simple value
                         else {
-                            Console.WriteLine($"{values[0]} {values[1]} -> {converter.ConvertSimple(values[1], values[2], number)} {values[2]}");
+                            Console.WriteLine($"{values[0]} {values[1]} -> {converter.Convert(values[1], values[2], number)} {values[2]}");
                         }
                     }
                 } catch(Exception ex) {
