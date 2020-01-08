60f4953ede62d16fd86d01ec6a790a123b1d2fcb
Lux <luxuridegaming@gmail.com>
2020-01-08 17:10:48 +0100

    Formating

--- Converter.cs
+++ Converter.cs
@@ -7,32 +7,40 @@ namespace UnitConverter
 {
     public class Converter
     {
-        private Dictionary<string, Dictionary<string, double>> _config {get;set;}
-        public Converter(string config) {
+        private Dictionary<string, Dictionary<string, double>> _config { get; set; }
+
+        public Converter(string config)
+        {
             _config = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, double>>>(config);
         }
+
         // Core of converter
         // Does basic convert and conversion between units of different type
-        public double Convert(string from, string to, double number){
+        public double Convert(string from, string to, double number)
+        {
             try
             {
                 //Get Unit types
-                string fromType = _config.First(x => x.Value.ContainsKey(from.Split("countper")[0])).Key;
-                string fromTypePer = (from.Split("countper").Length == 2) ? _config.First(x => x.Value.ContainsKey(from.Split("countper")[1])).Key : "";
+                var fromType = _config.First(x => x.Value.ContainsKey(from.Split("countper")[0])).Key;
+                var fromTypePer = @from.Split("countper").Length == 2
+                    ? _config.First(x => x.Value.ContainsKey(from.Split("countper")[1])).Key
+                    : "";
 
-                string toType = _config.First(x => x.Value.ContainsKey(to.Split("countper")[0])).Key;
-                string toTypePer = (to.Split("countper").Length == 2) ? _config.First(x => x.Value.ContainsKey(to.Split("countper")[1])).Key : "";
+                var toType = _config.First(x => x.Value.ContainsKey(to.Split("countper")[0])).Key;
+                var toTypePer = to.Split("countper").Length == 2
+                    ? _config.First(x => x.Value.ContainsKey(to.Split("countper")[1])).Key
+                    : "";
 
                 //Advanced unit conversion
                 if (_config.ContainsKey($"{fromType}countto{toType}") ||
                     _config.ContainsKey($"{toType}countto{fromType}"))
                 {
                     //Get advanced convert unit type
-                    string advancedType = _config.First(x =>
+                    var advancedType = _config.First(x =>
                         x.Key == $"{fromType}countto{toType}" || x.Key == $"{toType}countto{fromType}").Key;
                     //Get Units
-                    string fromBasic = _config[advancedType].First(x => _config[fromType].ContainsKey(x.Key)).Key;
-                    string toBasic = _config[advancedType].First(x => _config[toType].ContainsKey(x.Key)).Key;
+                    var fromBasic = _config[advancedType].First(x => _config[fromType].ContainsKey(x.Key)).Key;
+                    var toBasic = _config[advancedType].First(x => _config[toType].ContainsKey(x.Key)).Key;
                     return Convert(toBasic, to, Convert(from, fromBasic, number)) * _config[advancedType][toBasic] /
                            _config[advancedType][fromBasic];
                 }
@@ -40,16 +48,16 @@ namespace UnitConverter
                 else if (_config.ContainsKey($"{fromType}countto{toType}countper{toTypePer}") ||
                          _config.ContainsKey($"{toType}countto{fromType}countper{fromTypePer}"))
                 {
-                    string advancedType = _config.First(x =>
+                    var advancedType = _config.First(x =>
                         x.Key == $"{fromType}countto{toType}countper{toTypePer}" ||
                         x.Key == $"{toType}countto{fromType}countper{fromTypePer}").Key;
-                    string fromBasic = _config[advancedType]
+                    var fromBasic = _config[advancedType]
                         .First(x => _config[fromType].ContainsKey(x.Key.Split("countper")[0])).Key.Split("countper")[0];
-                    string toBasic = _config[advancedType]
+                    var toBasic = _config[advancedType]
                         .First(x => _config[toType].ContainsKey(x.Key.Split("countper")[0])).Key.Split("countper")[0];
                     if (fromTypePer != "")
                     {
-                        string fromBasicPer = _config[advancedType].First(x =>
+                        var fromBasicPer = _config[advancedType].First(x =>
                             x.Key.Split("countper").Length == 2 &&
                             _config[fromTypePer].ContainsKey(x.Key.Split("countper")[1])).Key.Split("countper")[1];
                         return Convert(toBasic, to,
@@ -59,11 +67,11 @@ namespace UnitConverter
 
                     if (toTypePer != "")
                     {
-                        string toBasicPer = _config[advancedType].First(x =>
+                        var toBasicPer = _config[advancedType].First(x =>
                             x.Key.Split("countper").Length == 2 &&
                             _config[toTypePer].ContainsKey(x.Key.Split("countper")[1])).Key.Split("countper")[1];
                         return Convert(from, fromBasic, 1) / Convert(to.Split("countper")[0], toBasic, 1) *
-                               Convert(to.Split("countper")[1], toBasicPer, 1)  * number;
+                               Convert(to.Split("countper")[1], toBasicPer, 1) * number;
                     }
                 }
                 //Basic conversion
@@ -71,13 +79,27 @@ namespace UnitConverter
                 {
                     return number / _config[toType][to] * _config[fromType][from];
                 }
-            } catch{}
+            }
+            catch
+            {
+            }
+
             //If there is no conversion path return not convertable message
             throw new ArgumentException($"Can't convert {from} to {to}");
         }
+
         //Converts Unit/Unit to Unit/Unit
-        public double Convert(string fromFirst, string toFirst, string fromSecond, string toSecond, double number) => number * Convert(fromFirst, toFirst, 1) / Convert(fromSecond, toSecond, 1);
+        public double Convert(string fromFirst, string toFirst, string fromSecond, string toSecond, double number)
+        {
+            return number * Convert(fromFirst, toFirst, 1) / Convert(fromSecond, toSecond, 1);
+        }
+
         //Converts Unit to Unit/Unit or Unit/Unit to Unit
-        public double Convert(string first, string second, string third, double number, int pos) => (pos == 0) ? Convert($"{first}countper{second}", third, number) : Convert(first, $"{second}countper{third}", number);
+        public double Convert(string first, string second, string third, double number, int pos)
+        {
+            return pos == 0
+                ? Convert($"{first}countper{second}", third, number)
+                : Convert(first, $"{second}countper{third}", number);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,36 +5,52 @@ using System.Threading.Tasks;
 
 namespace UnitConverter
 {
-    class Program
+    internal class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Input templates:\nSimple convert - number fromUnit toUnit\nAdvanced convert - number fromUnit1/fromUnit2 toUnit1/toUnit2");
+            Console.WriteLine(
+                "Input templates:\nSimple convert - number fromUnit toUnit\nAdvanced convert - number fromUnit1/fromUnit2 toUnit1/toUnit2");
             var userInput = "";
             // When you create converter you send him config
-            Converter converter = new Converter(await File.ReadAllTextAsync("./config.json"));
-            while((userInput = Console.ReadLine().ToLower()) != "exit") {
+            var converter = new Converter(await File.ReadAllTextAsync("./config.json"));
+            while ((userInput = Console.ReadLine().ToLower()) != "exit")
+            {
                 var values = userInput.Split(' ');
-                try {
-                    if(double.TryParse(values[0], out double number)) {
+                try
+                {
+                    if (double.TryParse(values[0], out var number))
+                    {
                         // Convert advanced value
-                        if(values[1].Split('/').Length == 2 && values[2].Split('/').Length == 2) {
+                        if (values[1].Split('/').Length == 2 && values[2].Split('/').Length == 2)
+                        {
                             var values1 = values[1].Split('/');
                             var values2 = values[2].Split('/');
-                            Console.WriteLine($"{values[0]} {values[1]} -> {converter.Convert(values1[0], values2[0], values1[1], values2[1], number)} {values[2]}");
-                        } else if(values[1].Split('/').Length == 2) {
+                            Console.WriteLine(
+                                $"{values[0]} {values[1]} -> {converter.Convert(values1[0], values2[0], values1[1], values2[1], number)} {values[2]}");
+                        }
+                        else if (values[1].Split('/').Length == 2)
+                        {
                             var anotherValues = values[1].Split('/');
-                            Console.WriteLine($"{values[0]} {values[1]} -> {converter.Convert(anotherValues[0], anotherValues[1], values[2], number, 0)} {values[2]}");
-                        } else if(values[2].Split('/').Length == 2) {
+                            Console.WriteLine(
+                                $"{values[0]} {values[1]} -> {converter.Convert(anotherValues[0], anotherValues[1], values[2], number, 0)} {values[2]}");
+                        }
+                        else if (values[2].Split('/').Length == 2)
+                        {
                             var anotherValues = values[2].Split('/');
-                            Console.WriteLine($"{values[0]} {values[1]} -> {converter.Convert(values[1], anotherValues[0], anotherValues[1], number, 1)} {values[2]}");
+                            Console.WriteLine(
+                                $"{values[0]} {values[1]} -> {converter.Convert(values[1], anotherValues[0], anotherValues[1], number, 1)} {values[2]}");
                         }
                         // Convert simple value
-                        else {
-                            Console.WriteLine($"{values[0]} {values[1]} -> {converter.Convert(values[1], values[2], number)} {values[2]}");
+                        else
+                        {
+                            Console.WriteLine(
+                                $"{values[0]} {values[1]} -> {converter.Convert(values[1], values[2], number)} {values[2]}");
                         }
                     }
-                } catch(Exception ex) {
+                }
+                catch (Exception ex)
+                {
                     Console.WriteLine(ex.Message);
                 }
             }
